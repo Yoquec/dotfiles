f7527692a21e6f7f6b9e0cce63499d12f9f03225
Alvaro Viejo <alvaro.viejo@yoquec.com>
2025-07-26 02:50:30 +0200

    chore: manage fonts with nix

--- modules/home/fonts.nix
+++ modules/home/fonts.nix
@@ -1,13 +1,44 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: {
   options.modules.fontconfig.enable = lib.mkEnableOption "Manage font-config configurations with nix";
 
-  conifg = lib.mkIf config.modules.fontconfig.enable {
+  config = lib.mkIf config.modules.fontconfig.enable {
     fonts.fontconfig = {
       enable = true;
+      defaultFonts = {
+        emoji = ["Noto Color Emoji"];
+        monospace = ["IntoneMono Nerd Font Mono"];
+        serif = [
+          "Libertinus Serif Display"
+          "Noto Serif CJK SC"
+          "Noto Serif CJK TC"
+          "Noto Serif CJK JP"
+        ];
+        sansSerif = [
+          "Inter Display"
+          "Noto Sans CJK SC"
+          "Noto Sans CJK TC"
+          "Noto Sans CJK JP"
+        ];
+      };
     };
+
+    home.packages = with pkgs; [
+      # Serif
+      libertinus
+      noto-fonts-cjk-serif
+
+      # Sans
+      inter
+      noto-fonts-color-emoji
+      noto-fonts-cjk-sans
+
+      # Monospace
+      nerd-fonts.intone-mono
+    ];
   };
 }
