76d14d0e432f5df3ef371ed64034a8ccf2d6cc9f
Alvaro Viejo <alvaro.viejo@yoquec.com>
2025-08-17 13:33:14 +0200

    Revert "fix: optionally enable tms keybinds"
    This reverts commit f5128fa7d5834d25324ee9e0efb3e678af9480ec.

--- modules/home/development/tmux.nix
+++ modules/home/development/tmux.nix
@@ -32,13 +32,11 @@
   extraConfig = ''
     set -sg escape-time 0
 
-    ${lib.optionalString config.modules.development.tmux.tmsKeymaps ''
-      # Keybinds
-      bind-key W run-shell "${tmuxPackages.tms}/bin/tms '$WIKI_HOME' 'Wiki 📚'"
-      bind-key H run-shell "${tmuxPackages.tms}/bin/tms '$HOME' 'Home 🏠'"
-      bind-key N popup -E "${tmuxPackages.tmsproject}/bin/tmsproject"
-      bind-key Space popup -E "${tmuxPackages.tmswitch}/bin/tmswitch"
-    ''}
+    # Keybinds
+    bind-key W run-shell "${tmuxPackages.tms}/bin/tms '$WIKI_HOME' 'Wiki 📚'"
+    bind-key H run-shell "${tmuxPackages.tms}/bin/tms '$HOME' 'Home 🏠'"
+    bind-key N popup -E "${tmuxPackages.tmsproject}/bin/tmsproject"
+    bind-key Space popup -E "${tmuxPackages.tmswitch}/bin/tmswitch"
 
     # Colored undercurls
     set -ga terminal-overrides ',*256col*:Tc'
@@ -65,12 +63,9 @@
 in {
   options.modules.development.tmux = {
     enable = lib.mkEnableOption "Enable tmux";
-    tmsKeymaps = lib.mkEnableOption "Adds keymap configuration for tms scripts";
   };
 
   config = lib.mkIf config.modules.development.tmux.enable {
-    modules.development.tmux.tmsKeymaps = lib.mkDefault true;
-
     programs.tmux = {
       enable = true;
       clock24 = true;
