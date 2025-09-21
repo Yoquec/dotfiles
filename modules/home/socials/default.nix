0586b0ceee0befcf39047d21a9c2e5656a110035
Alvaro Viejo <alvaro.viejo@yoquec.com>
2025-09-21 15:01:31 +0200

    Revert "chore: adds disneyplus desktop application"
    This reverts commit e35add21713e67ff3f8644c0369e3fbc026d4d2f.

--- modules/home/socials/default.nix
+++ modules/home/socials/default.nix
@@ -10,7 +10,6 @@ in
   imports = [
     ./whatsapp.nix
     ./discord.nix
-    ./disneyplus.nix
   ];
 
   options.modules.socials.enable = lib.mkEnableOption "Enable social media bundle";
@@ -19,6 +18,5 @@ in
     enable = lib.mkDefault true;
     whatsapp.enable = lib.mkDefault enable;
     discord.enable = lib.mkDefault enable;
-    disneyplus.enable = lib.mkDefault enable;
   };
 }
