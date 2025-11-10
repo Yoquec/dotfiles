9746fefa059aa97e744d550670d0d1ebbca4725b
Alvaro Viejo <alvaro.viejo@yoquec.com>
2025-11-10 17:37:16 +0100

    fix(emaview): uses emanote layers instead of `cd`

--- packages/emaview.nix
+++ packages/emaview.nix
@@ -11,8 +11,7 @@ writeShellScriptBin "emaview" ''
       kill $(jobs -p)
   }
 
-  cd $WIKI_HOME
-  ${emanote}/bin/emanote run --port $PORT &
+  ${emanote}/bin/emanote -L $WIKI_HOME run --port $PORT &
   ${ungoogled-chromium}/bin/chromium --user-data-dir=$HOME/.cache/chromium/emaview --app="http://127.0.0.1:$PORT"
 
   trap cleanup EXIT
