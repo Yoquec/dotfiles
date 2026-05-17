60fd7d96ed35186ec1c0043c3cef5b0560376d65
Alvaro Viejo <alvaro.viejo@yoquec.com>
2026-05-17 16:03:30 +0200

    chore(socials): wires module with autowiring

--- modules/home/socials/default.nix
+++ modules/home/socials/default.nix
@@ -1,24 +1,5 @@
-{
-  lib,
-  config,
-  ...
-}:
-let
-  inherit (config.modules.socials) enable;
-in
-{
-  imports = [
-    ./whatsapp.nix
-    ./discord.nix
-    ./netflix.nix 
-  ];
-
-  options.modules.socials.enable = lib.mkEnableOption "Enable social media bundle";
-
-  config.modules.socials = {
-    enable = lib.mkDefault false;
-    whatsapp.enable = lib.mkDefault enable;
-    discord.enable = lib.mkDefault enable;
-    netflix.enable = lib.mkDefault enable;
-  };
+{ flake, lib, ... }:
+import "${flake.inputs.self}/lib/autowire/mkModule.nix" { inherit lib; } {
+  path = ./.;
+  name = "socials";
 }
