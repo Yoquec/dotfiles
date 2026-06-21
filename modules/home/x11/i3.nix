c5180ddc96d7c34c67d13d9331cf2cef08f2e15d
Alvaro Viejo <alvaro.viejo@yoquec.com>
2026-06-21 12:15:54 +0200

    refactor(i3): patches i3blocks config instead of exposing binaries to PATH

--- modules/home/x11/i3.nix
+++ modules/home/x11/i3.nix
@@ -8,12 +8,28 @@ let
   inherit (config) xdg;
   inherit (config.modules.x11) i3;
 
+  volume-i3blocks = pkgs.writeShellScriptBin "volume-i3blocks" (
+    builtins.readFile ../../../dotfiles/i3blocks/bin/volume-i3blocks
+  );
+  battery-i3blocks = pkgs.writeShellScriptBin "battery-i3blocks" (
+    builtins.readFile ../../../dotfiles/i3blocks/bin/battery-i3blocks
+  );
+
   # Patch the i3 config file to add the correct wallpaper
-  patched = pkgs.runCommand "i3-config-patched" { } ''
+  i3Patched = pkgs.runCommand "i3-config-patched" { } ''
     cp -r ${../../../dotfiles/i3} $out
     chmod -R u+w $out
     substituteInPlace $out/config \
       --replace "wallpaper.png" "${../../../assets/wallpapers/charcoal_creation_of_adam.png}"
+  '';
+
+  i3BlocksPatched = pkgs.runCommand "i3blocks-config-patched" { } ''
+    mkdir "$out"
+    cp "${../../../dotfiles/i3blocks}/config" "$out"
+    chmod -R u+w "$out"
+    substituteInPlace "$out/config" \
+      --replace "volume-i3blocks" "${lib.getExe volume-i3blocks}" \
+      --replace "battery-i3blocks" "${lib.getExe battery-i3blocks}"
   '';
 in
 {
@@ -23,29 +39,18 @@ in
   };
 
   config = lib.mkIf i3.enable {
-    # TODO: i3 should be installed through home-manager once NixOS is installed
-
     home.file = {
-      "${xdg.configHome}/i3".source = patched;
-      "${xdg.configHome}/i3blocks/config".source = ../../../dotfiles/i3blocks/config;
+      "${xdg.configHome}/i3".source = i3Patched;
+      "${xdg.configHome}/i3blocks".source = i3BlocksPatched;
     };
 
-    home.packages = [
-      (pkgs.writeShellScriptBin "volume-i3blocks" (
-        builtins.readFile ../../../dotfiles/i3blocks/bin/volume-i3blocks
-      ))
-      (pkgs.writeShellScriptBin "battery-i3blocks" (
-        builtins.readFile ../../../dotfiles/i3blocks/bin/battery-i3blocks
-      ))
-    ]
-    ++ (
-      if i3.installBinary then
-        (with pkgs; [
-          i3
-          i3blocks
-        ])
-      else
-        [ ]
+    # TODO: i3 should be installed through home-manager once NixOS is installed
+    home.packages = lib.mkIf i3.installBinary (
+      with pkgs;
+      [
+        i3
+        i3blocks
+      ]
     );
   };
 }
