fd3b9dcfaee5b8e5e07d0289b90bcb5001cc69da
Alvaro Viejo <alvaro.viejo@yoquec.com>
2026-07-18 01:23:36 +0200

    chore(soundcloud): implements jailed soundcloud using google-chrome for DRM

--- modules/home/media/soundcloud.nix
+++ modules/home/media/soundcloud.nix
@@ -1,20 +1,35 @@
 {
   lib,
+  jail,
   config,
   pkgs,
   ...
 }:
 let
+  inherit (config.home) homeDirectory;
   inherit (config.modules.media) soundcloud;
+
+  name = "soundcloud";
+  dataDir = "${homeDirectory}/.cache/chromium/${name}";
+  script = pkgs.writeShellScriptBin name ''
+    ${lib.getExe pkgs.google-chrome} --user-data-dir="${dataDir}" --app=https://www.netflix.com
+  '';
+
+  jailed = jail name script (
+    with jail.combinators;
+    [
+      unsafe-gui
+      (try-readwrite dataDir)
+      (add-runtime "mkdir -p ${dataDir}")
+      pulse
+      pipewire
+    ]
+  );
 in
 {
   options.modules.media.soundcloud = {
-    enable = lib.mkEnableOption "Enable soundcloud desktop application through chormium";
+    enable = lib.mkEnableOption "Enable ${name} desktop application through google chrome";
   };
 
-  config.home.packages = lib.mkIf soundcloud.enable [
-    (pkgs.writeShellScriptBin "soundcloud" ''
-      ${pkgs.ungoogled-chromium}/bin/chromium --user-data-dir=$HOME/.cache/chromium/soundcloud --app=https://soundcloud.com
-    '')
-  ];
+  config.home.packages = lib.mkIf soundcloud.enable [ jailed ];
 }
