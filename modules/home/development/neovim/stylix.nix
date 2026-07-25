1546eeaee2a725a9d4c133a1dddb65e25ffeb438
Alvaro Viejo <alvaro.viejo@yoquec.com>
2026-07-25 19:21:16 +0200

    fix(nixvim): fixes markdown link colors

--- modules/home/development/neovim/stylix.nix
+++ modules/home/development/neovim/stylix.nix
@@ -13,17 +13,25 @@
     };
 
     programs.nixvim.extraConfigLua = ''
-      local function override(src, overrides)
+      local function override(src, dest, overrides)
         local ok, base = pcall(vim.api.nvim_get_hl, 0, { name = src, link = false })
         if not ok then return end
         for k, v in pairs(overrides) do
           base[k] = v
         end
-        vim.api.nvim_set_hl(0, src, base)
+        vim.api.nvim_set_hl(0, dest, base)
+      end
+
+      local function link(src, dest)
+          vim.api.nvim_set_hl(0, dest, { link= src })
+      end
+
+      local function override_self(src, overrides)
+          override(src, src, overrides)
       end
 
       local function transparent(group)
-          override(group, { bg = "NONE" })
+          override_self(group, { bg = "NONE" })
       end
 
       transparent("StatusLine") 
@@ -53,6 +61,12 @@
 
       transparent("WhichKeyFloat")
       transparent("WhichKeySeparator")
+
+      link("@label", "@markup.link.label.markdown")
+      link("@tag", "@markup.link.label.markdown_inline")
+
+      override("@lsp.type.method", "@markup.link.markdown_inline", { underline = true })
+      override("@lsp.type.method", "@markup.link.url.markdown", { underline = true })
     '';
   };
 }
