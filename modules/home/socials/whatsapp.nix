4ce82a0036ff7c0894f48e6dfdc2499a76f67ddb
Alvaro Viejo <alvaro.viejo@yoquec.com>
2026-08-01 19:17:27 +0200

    chore(socials): adds notifications support

--- modules/home/socials/whatsapp.nix
+++ modules/home/socials/whatsapp.nix
@@ -21,6 +21,7 @@ let
     [
       gui
       network
+      notifications
       (try-readwrite dataDir)
       (add-runtime "mkdir -p ${dataDir}")
       (try-readwrite "${homeDirectory}/Downloads")
